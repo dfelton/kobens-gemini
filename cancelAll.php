39b491a86b2265f333db42718d9fa437068bb362
Darren Felton <darren.felton@bestbuy.com>
2019-01-28 13:23:24 -0600

    Revised include paths; obj instead of array

--- cancelAll.php
+++ cancelAll.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * Creates orders
+ * Cancels all orders on exchange server
  */
 
-require_once 'lib/cli.php';
-require_once ((bool) Cli::getArg('production')) ? 'env/production.php' : 'env/sandbox.php';
-require_once 'lib/cancelAll.php';
+require_once __DIR__.'/lib/cli.php';
+require_once __DIR__.'/lib/cancelAll.php';
+require_once __DIR__.'/lib/Zend/Debug.php';
+
+if ((string) Cli::getArg('env') === 'production') {
+    require_once __DIR__.'/env/production.php';
+} else {
+    require_once __DIR__.'/env/sandbox.php';
+}
 
 if ((bool) Cli::getArg('production')) {
     die("\n\tCannot cancel all orders in production.\n\n");
@@ -22,13 +28,11 @@ if (!is_string($response)) {
     throw new Exception('Unhandled Response');
 }
 
-$response = json_decode($response, true);
+$response = json_decode($response);
 
-if ($response['result'] === 'ok') {
-    echo "All Orders Cancelled.\n";
+if ($response->result === 'ok') {
+    echo "All orders cancelled.\n";
 } else {
     echo "There was a problem cancelling all orders.\n";
     Zend_Debug::dump($response, 'response');
 }
-
-Zend_Debug::dump($cancelAllOrders->getResponse());
