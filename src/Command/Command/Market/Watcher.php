2e2a9d69b022c17308b56edd061f3bffefa0555a
Darren Felton <2659558+dfelton@users.noreply.github.com>
2019-03-10 16:01:19 -0500

    Faster refresh when closed; much better output formatting

--- src/Command/Command/Market/Watcher.php
+++ src/Command/Command/Market/Watcher.php
@@ -5,16 +5,17 @@ namespace Kobens\Gemini\Command\Command\Market;
 use Kobens\Core\Config;
 use Kobens\Exchange\Exception\ClosedBookException;
 use Kobens\Gemini\Command\Argument\{RefreshRate, Symbol};
-use Kobens\Gemini\Command\Traits\{CommandTraits, GetRefreshRate};
+use Kobens\Gemini\Command\Traits\{CommandTraits, GetRefreshRate, GetSymbol};
 use Kobens\Gemini\Exchange;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Kobens\Gemini\Api\Host;
 
 class Watcher extends Command
 {
     use CommandTraits;
-    use GetRefreshRate;
+    use GetRefreshRate, GetSymbol;
 
     protected static $defaultName = 'market:watcher';
 
@@ -26,9 +27,9 @@ class Watcher extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $book = (new Exchange())->getBook($input->getArgument('symbol'));
-        $host = (new Config())->gemini->api->host;
-        $refreshRate = $this->getRefreshRate($input, $output);
+        $symbol = $this->getSymbol($input)->getValue();
+        $book = (new Exchange())->getBook($symbol);
+        $refreshRate = $this->getRefreshRate($input, $output)->getValue();
         $lastAsk = null;
         $lastBid = null;
         $lastOutput = 0;
@@ -46,12 +47,12 @@ class Watcher extends Command
                     $bookIsOpen = false;
                     $this->clearTerminal($output);
                     $output->write(\sprintf(
-                        'Book "%s" on "%s" is closed.',
+                        '<fg=red>Book "%s" on "%s" is closed.</>',
                         $book->getSymbol(),
-                        $host
+                        $this->getHost()
                     ));
                 }
-                $this->sleep($output);
+                $this->sleep($output, 1);
                 continue;
             }
 
@@ -60,20 +61,43 @@ class Watcher extends Command
                 $lastOutput = $time;
                 $lastAsk = $ask;
                 $lastBid = $bid;
-                $this->clearTerminal($output);
-                $output->writeln([
-                    "Date:\t".$this->getNow(),
-                    "Host:\t".$host,
-                    "Symbol:\t".$book->getSymbol(),
-                    "Bid:\t".$bid,
-                    "Ask:\t".$ask,
-                    "Spread:\t".$spread,
-                    PHP_EOL,
-                ]);
+                $this->outputUpdate($output, $symbol, $bid, $ask, $spread);
             }
             $bookIsOpen = true;
-            \usleep($refreshRate->getValue());
+            \usleep($refreshRate);
         }
+    }
+
+    protected function outputUpdate(OutputInterface $output, string $symbol, string $bid, string $ask, string $spread)
+    {
+
+        $tabsBid = 3 - \floor(\strlen($bid)/8);
+        if ($tabsBid < 0) {
+            $tabsBid = 0;
+        }
+
+        $tabsAsk = 3 - \floor(\strlen($ask)/8);
+        if ($tabsAsk < 0) {
+            $tabsAsk = 0;
+        }
+
+        $tabsSpread = 3 - \floor(\strlen($spread)/8);
+        if ($tabsSpread < 0) {
+            $tabsSpread = 0;
+        }
+
+        $this->clearTerminal($output);
+        $output->writeln([
+            \str_repeat('-', 41),
+            \sprintf("- Date:\t\t%s\t-", $this->getNow()),
+            \sprintf("- Host:\t\t%s\t-", $this->getHost()),
+            \sprintf("- Symbol:\t%s\t\t\t-", $symbol),
+            "-\t\t\t\t\t-",
+            \sprintf("- Ask:\t\t<fg=red>%s</>%s-", $ask, \str_repeat("\t", $tabsAsk)),
+            \sprintf("- Bid:\t\t<fg=green>%s</>%s-", $bid, \str_repeat("\t", $tabsBid)),
+            \sprintf("- Spread:\t%s%s-", $spread, \str_repeat("\t", $tabsSpread)),
+            \str_repeat('-', 41),
+        ]);
     }
 
 }
