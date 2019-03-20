43de56c75b92d3bf3eec08efcb6ce6f89e9c9119
Darren Felton <2659558+dfelton@users.noreply.github.com>
2019-03-19 20:17:12 -0500

    Updated kobens/kobens-exchange; added new interface methods

--- src/Exchange.php
+++ src/Exchange.php
@@ -5,9 +5,11 @@ namespace Kobens\Gemini;
 use Kobens\Exchange\AbstractExchange;
 use Kobens\Exchange\Book\Keeper\KeeperInterface;
 use Kobens\Gemini\Api\WebSocket\MarketData\BookKeeper;
-use Kobens\Gemini\Exchange\Pair\{BTCUSD, ETHBTC, ETHUSD, LTCBTC, LTCETH, LTCUSD, ZECBTC, ZECETH, ZECLTC, ZECUSD};
 use Kobens\Gemini\Api\Param\{Amount, ClientOrderId, Price, Side, Symbol};
 use Kobens\Gemini\Api\Rest\Request\Order\Placement\NewOrder;
+use Kobens\Gemini\Api\Rest\Request\Order\Status\{ActiveOrders, OrderStatus};
+use Kobens\Gemini\Exception\Exception;
+use Kobens\Gemini\Exchange\Pair\{BTCUSD, ETHBTC, ETHUSD, LTCBTC, LTCETH, LTCUSD, ZECBTC, ZECETH, ZECLTC, ZECUSD};
 
 class Exchange extends AbstractExchange
 {
@@ -53,6 +55,38 @@ class Exchange extends AbstractExchange
         );
         $result = \json_decode($order->getResponse()['body']);
         return $result->order_id;
+    }
+
+    public function isOrderCancelled(array $metaData): bool
+    {
+        if (!isset($metaData['is_cancelled'])) {
+            throw new Exception('Metadata array missing "is_cancelled" information');
+        }
+        return $metaData['is_cancelled'] === true;
+    }
+
+    public function getActiveOrderIds(): array
+    {
+        $orders = \json_decode((new ActiveOrders())->getResponse()['body'], true);
+        $orderIds = [];
+        foreach ($orders as $order) {
+            $orderIds[] = $order['order_id'];
+        }
+        return $orderIds;
+    }
+
+    public function getOrderMetaData(string $orderId): array
+    {
+        $json = (new OrderStatus($orderId))->getResponse();
+        return \json_decode($json['body'], true);
+    }
+
+    public function isOrderFilled(array $metaData): bool
+    {
+        if (!isset($metaData['is_live']) || !isset($metaData['remaining_amount'])) {
+            throw new Exception('Metadata missing required keys.');
+        }
+        return $metaData['is_live'] === false && $metaData['remaining_amount'] === '0';
     }
 
 }
