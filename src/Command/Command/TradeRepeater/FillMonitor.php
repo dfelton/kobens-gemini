896ebe2048cc2838cdd26cda708115c2b6f28ac9
Darren Felton <darren.felton@franklincovey.com>
2019-10-22 22:44:42 -0500

    More usage of DI and Interfaces; Revised logic a bit; Implemented EmergencyShutdownInterface

--- src/Command/Command/TradeRepeater/FillMonitor.php
+++ src/Command/Command/TradeRepeater/FillMonitor.php
@@ -2,13 +2,13 @@
 
 namespace Kobens\Gemini\Command\Command\TradeRepeater;
 
-use Amp\Loop;
 use Amp\Websocket\Client\Handshake;
+use Kobens\Core\EmergencyShutdownInterface;
 use Kobens\Gemini\Api\Host;
-use Kobens\Gemini\Api\Key;
-use Kobens\Gemini\Api\Nonce;
-use Kobens\Gemini\TradeRepeater\DataResource\BuyPlaced;
-use Kobens\Gemini\TradeRepeater\DataResource\SellPlaced;
+use Kobens\Gemini\Api\KeyInterface;
+use Kobens\Gemini\Api\NonceInterface;
+use Kobens\Gemini\TradeRepeater\DataResource\BuyFilledInterface;
+use Kobens\Gemini\TradeRepeater\DataResource\SellFilledInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,25 +17,71 @@ final class FillMonitor extends Command
 {
     protected static $defaultName = 'trade-repeater:fill-monitor';
 
+    /**
+     * @var BuyFilledInterface
+     */
     private $buyFilled;
 
+    /**
+     * @var SellFilledInterface
+     */
     private $sellFilled;
+
+    /**
+     * @var KeyInterface
+     */
+    private $key;
+
+    /**
+     * @var NonceInterface
+     */
+    private $nonce;
+
+    /**
+     * @var EmergencyShutdownInterface
+     */
+    private $shutdown;
+
+    public function __construct(
+        BuyFilledInterface $buyFilledInterface,
+        SellFilledInterface $sellFilledInterface,
+        NonceInterface $nonceInterface,
+        KeyInterface $keyInterface,
+        EmergencyShutdownInterface $shutdownInterface
+    ) {
+        parent::__construct();
+        $this->buyFilled = $buyFilledInterface;
+        $this->sellFilled = $sellFilledInterface;
+        $this->key = $keyInterface;
+        $this->nonce = $nonceInterface;
+        $this->shutdown = $shutdownInterface;
+    }
 
     protected function configure()
     {
         $this->setDescription('Monitors order fillings for the Gemini Trade Repater');
     }
 
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-        $this->buyFilled = new BuyPlaced();
-        $this->sellFilled = new SellPlaced();
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        Loop::run(function () use ($output)
-        {
+        while (!$this->shutdown->isShutdownModeEnabled()) {
+            try {
+                \Amp\Loop::run($this->main($output));
+            } catch (\Exception $e) {
+                $this->shutdown->enableShutdownMode(\json_encode([
+                    'time' => $this->now(),
+                    'message' => $e->getMessage(),
+                    'code' => $e->getCode(),
+                    'class' => \get_class($e),
+                    'trace' => $e->getTraceAsString()
+                ]));
+            }
+        }
+    }
+
+    private function main(OutputInterface $output): \Closure
+    {
+        return function() use ($output) {
             /** @var \Amp\Websocket\Client\Connection $connection */
             /** @var \Amp\Websocket\Message $message */
             $connection = yield \Amp\Websocket\Client\connect(
@@ -53,22 +99,22 @@ final class FillMonitor extends Command
                     $this->processMessage($data, $output);
                 }
             }
-        });
+        };
     }
 
     private function processMessage(array $msg, OutputInterface $output): void
     {
         switch (true) {
             case $msg['type'] === 'subscription_ack':
-                $output->writeln((new \DateTime())->format('Y-m-d H:i:s')."\tSubscription acknowledged.");
+                $output->writeln($this->now()."\tSubscription acknowledged.");
                 break;
             case $msg['type'] === 'heartbeat':
-                $output->writeln((new \DateTime())->format('Y-m-d H:i:s')."\tHeartbeat Received");
+                $output->writeln($this->now()."\tHeartbeat Received");
                 break;
             case $msg['type'] === 'fill' && $msg['remaining_amount'] !== '0':
                 $output->writeln(\sprintf(
                     "%s\t<fg=yellow>Partial</> fill of <fg=%s>%s</> order %s. Executed <fg=yellow>%s</>. Remaining amount: <fg=yellow>%s</>",
-                    (new \DateTime())->format('Y-m-d H:i:s'),
+                    $this->now(),
                     $msg['side']==='buy'?'green':'red',
                     $msg['side'],
                     $msg['order_id'],
@@ -81,18 +127,17 @@ final class FillMonitor extends Command
                 if ($repeaterId) {
                     if ($msg['side'] === 'buy') {
                         $this->buyFilled->setNextState($repeaterId);
-                        $output->writeln((new \DateTime())->format('Y-m-d H:i:s')."\t($repeaterId) Buy order {$msg['order_id']} on {$msg['symbol']} pair for {$msg['original_amount']} at price of {$msg['price']} filled.");
+                        $output->writeln($this->now()."\t($repeaterId) Buy order {$msg['order_id']} on {$msg['symbol']} pair for {$msg['original_amount']} at price of {$msg['price']} filled.");
                     } elseif ($msg['side'] === 'sell') {
                         $this->sellFilled->setNextState($repeaterId);
-                        $output->writeln((new \DateTime())->format('Y-m-d H:i:s')."\t($repeaterId) Sell order {$msg['order_id']} on {$msg['symbol']} pair for {$msg['original_amount']} at price of {$msg['price']} filled.");
+                        $output->writeln($this->now()."\t($repeaterId) Sell order {$msg['order_id']} on {$msg['symbol']} pair for {$msg['original_amount']} at price of {$msg['price']} filled.");
                     } else {
                         throw new \Exception("Unhandled side '{$msg['side']}'");
                     }
                 }
                 break;
             default:
-                \Zend\Debug\Debug::dump($msg);
-                throw new \Exception('Unhandled Message');
+                throw new \Exception('Unhandled Message: '.\json_encode($msg));
         }
     }
 
@@ -113,18 +158,21 @@ final class FillMonitor extends Command
 
     private function getHeaders(): array
     {
-        $key = new Key();
         $payload = [
             'request' => '/v1/order/events',
-            'nonce' => (new Nonce())->getNonce()
+            'nonce' => $this->nonce->getNonce()
         ];
         $base64Payload = \base64_encode(\json_encode($payload));
-        $signature = \hash_hmac('sha384', $base64Payload, $key->getSecretKey());
+        $signature = \hash_hmac('sha384', $base64Payload, $this->key->getSecretKey());
         return [
-            'X-GEMINI-APIKEY'    => $key->getPublicKey(),
+            'X-GEMINI-APIKEY'    => $this->key->getPublicKey(),
             'X-GEMINI-PAYLOAD'   => $base64Payload,
             'X-GEMINI-SIGNATURE' => $signature,
         ];
     }
 
+    private function now(): string
+    {
+        return (new \DateTime())->format('Y-m-d H:i:s');
+    }
 }
