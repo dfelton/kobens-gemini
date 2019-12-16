182cbd4c0c20bb248b4f24b46ccd7b9f3b86c7a1
Darren Felton <darren.felton@franklincovey.com>
2019-12-15 21:23:01 -0600

    Limit command... really this belongs a few commits back

---
+++ src/Command/Command/OrderPlacement/NewOrder/Limit.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kobens\Gemini\Command\Command\OrderPlacement\NewOrder;
+
+use Kobens\Gemini\Api\Rest\PrivateEndpoints\OrderPlacement\NewOrder\LimitInterface;
+use Kobens\Gemini\Api\Rest\PrivateEndpoints\OrderPlacement\NewOrder\NewOrderInterface;
+
+final class Limit extends AbstractNewOrder
+{
+    protected static $defaultName = 'order-placement:new-order:limit';
+
+    /**
+     * @var LimitInterface
+     */
+    private $limitOrder;
+
+    public function __construct(
+        LimitInterface $limitInterface
+    ) {
+        $this->limitOrder = $limitInterface;
+        $this->setDescription('Places a new <fg=yellow>limit</> order on the exchange.');
+        parent::__construct();
+    }
+
+    protected function getNewOrderInterface(): NewOrderInterface
+    {
+        return $this->limitOrder;
+    }
+}
