4c10675f305fa3059d3321d738407edd10ee474e
Darren Felton <darren.felton@franklincovey.com>
2019-12-26 13:23:53 -0600

    Removed comment that is old an no longer applicable

--- src/Api/Nonce.php
+++ src/Api/Nonce.php
@@ -6,8 +6,8 @@ class Nonce implements NonceInterface
 {
     public function getNonce(): string
     {
-        // TODO ensure max of 600 per second by using db table 'nonce'
         $microtime = explode(' ', microtime());
         return $microtime[1].substr($microtime[0], 2);
     }
 }
+
