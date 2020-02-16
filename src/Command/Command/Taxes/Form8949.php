b313de1c7ee47ba33497f5cfc6691969ce3d839e
Darren Felton <darren.felton@thegrommet.com>
2020-02-15 19:27:21 -0600

    Improved Form 8949
    - Support for additional currency pairs added
    - Provide total cost basis and proceeds on output
    - Provide first buy date and last sell date on output

--- src/Command/Command/Taxes/Form8949.php
+++ src/Command/Command/Taxes/Form8949.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zend\Db\Sql\Select;
 use Zend\Db\TableGateway\TableGateway;
+use Symfony\Component\Console\Input\InputOption;
+use Kobens\Gemini\Exchange\Currency\Pair;
 
 final class Form8949 extends Command
 {
@@ -28,19 +30,30 @@ final class Form8949 extends Command
     protected function configure()
     {
         $this->addArgument('year', InputArgument::REQUIRED);
+        $this->addOption('symbol', 's', InputOption::VALUE_OPTIONAL, 'Trading Symbol', 'btcusd');
         $this->setDescription('Generates data for a US Federal Tax Form 8949.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->symbol = Pair::getInstance($input->getOption('symbol'))->getSymbol();
         $capitalGains = [
-            'assetsSold' => '0',
+            'assetsSold'    => '0',
             'totalProceeds' => '0',
 
-            'short' => [],
-            'long'  => [],
-            'shortTotal' => '0',
-            'longTotal'  => '0',
+            'long_count'         => 0,
+            'long_capital_gain'  => '0',
+            'long_cost_basis'    => '0',
+            'long_date_purchase' => null,
+            'long_date_sale'     => null,
+            'long_proceeds'      => '0',
+
+            'short_count'         => 0,
+            'short_capital_gain'  => '0',
+            'short_cost_basis'    => '0',
+            'short_date_purchase' => null,
+            'short_date_sale'     => null,
+            'short_proceeds'      => '0',
         ];
 
         $i = 0;
@@ -49,17 +62,31 @@ final class Form8949 extends Command
                 $this->outputHeaders($output);
             }
 
-            $capitalGains['assetsSold'] = Add::getResult($capitalGains['assetsSold'], $row['amount']);
+            $capitalGains['assetsSold']    = Add::getResult($capitalGains['assetsSold'],    $row['amount']);
             $capitalGains['totalProceeds'] = Add::getResult($capitalGains['totalProceeds'], $row['proceeds']);
 
             if ($this->isLongTerm($row['buy_date'], $row['sell_date'])) {
-                $capitalGains['long'][] = $row;
-                $capitalGains['longTotal'] = Add::getResult($capitalGains['longTotal'], $row['capital_gain']);
-
-
+                ++$capitalGains['long_count'];
+                $capitalGains['long_capital_gain'] = Add::getResult($capitalGains['long_capital_gain'], $row['capital_gain']);
+                $capitalGains['long_cost_basis']   = Add::getResult($capitalGains['long_cost_basis'],   $row['cost_basis']);
+                $capitalGains['long_proceeds']     = Add::getResult($capitalGains['long_proceeds'],     $row['proceeds']);
+                if ($capitalGains['long_date_purchase'] === null || \strtotime($capitalGains['long_date_purchase']) > \strtotime($row['buy_date'])) {
+                    $capitalGains['long_date_purchase'] = $row['buy_date'];
+                }
+                if ($capitalGains['long_date_sale'] === null || \strtotime($capitalGains['long_date_sale']) < \strtotime($row['sell_date'])) {
+                    $capitalGains['long_date_sale'] = $row['sell_date'];
+                }
             } else {
-                $capitalGains['short'][] = $row;
-                $capitalGains['shortTotal'] = Add::getResult($capitalGains['shortTotal'], $row['capital_gain']);
+                ++$capitalGains['short_count'];
+                $capitalGains['short_capital_gain'] = Add::getResult($capitalGains['short_capital_gain'], $row['capital_gain']);
+                $capitalGains['short_cost_basis']   = Add::getResult($capitalGains['short_cost_basis'],   $row['cost_basis']);
+                $capitalGains['short_proceeds']     = Add::getResult($capitalGains['short_proceeds'],     $row['proceeds']);
+                if ($capitalGains['short_date_purchase'] === null || \strtotime($capitalGains['short_date_purchase']) > \strtotime($row['buy_date'])) {
+                    $capitalGains['short_date_purchase'] = $row['buy_date'];
+                }
+                if ($capitalGains['short_date_sale'] === null || \strtotime($capitalGains['short_date_sale']) < \strtotime($row['sell_date'])) {
+                    $capitalGains['short_date_sale'] = $row['sell_date'];
+                }
             }
 
             if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
@@ -74,15 +101,27 @@ final class Form8949 extends Command
             $i++;
         }
 
-        $output->write("\n\n");
-        $output->write(\sprintf("Total Short Term Capital Gains: %s\n", $capitalGains['shortTotal']));
-        $output->write(\sprintf("Total Short Term Trades: %d", \count($capitalGains['short'])));
         $output->write("\n");
-        $output->write(\sprintf("Total Long Term Capital Gains: %s\n", $capitalGains['longTotal']));
-        $output->write(\sprintf("Total Long Term Trades: %d\n", \count($capitalGains['long'])));
+        $output->write("<options=bold>Short Term</>\n");
+        $output->write(\sprintf("Trades:\t\t%d\n",      $capitalGains['short_count']));
+        $output->write(\sprintf("Cost Basis:\t%s\n",    $capitalGains['short_cost_basis']));
+        $output->write(\sprintf("Proceeds:\t%s\n",      $capitalGains['short_proceeds']));
+        $output->write(\sprintf("Capital Gains:\t%s\n", $capitalGains['short_capital_gain']));
+        $output->write(\sprintf("Date Purchase:\t%s\n", $capitalGains['short_date_purchase']));
+        $output->write(\sprintf("Date Sell:\t%s\n",     $capitalGains['short_date_sale']));
         $output->write("\n");
-        $output->write(\sprintf("Total Proceeds: %s\n", $capitalGains['totalProceeds']));
-        $output->write(\sprintf("Total Assets Sold: %s\n", $capitalGains['assetsSold']));
+
+        $output->write("<options=bold>Long Term</>\n");
+        $output->write(\sprintf("Trades:\t\t%d\n",      $capitalGains['long_count']));
+        $output->write(\sprintf("Cost Basis:\t%s\n",    $capitalGains['long_cost_basis']));
+        $output->write(\sprintf("Proceeds:\t%s\n",      $capitalGains['long_proceeds']));
+        $output->write(\sprintf("Capital Gains:\t%s\n", $capitalGains['long_capital_gain']));
+        $output->write(\sprintf("Date Purchase:\t%s\n", $capitalGains['long_date_purchase']));
+        $output->write(\sprintf("Date Sell:\t%s\n",     $capitalGains['long_date_sale']));
+        $output->write("\n");
+
+        $output->write(\sprintf("Total Proceeds:\t\t%s\n",  $capitalGains['totalProceeds']));
+        $output->write(\sprintf("Total Assets Sold:\t%s\n", $capitalGains['assetsSold']));
         $output->write("\n");
     }
 
@@ -111,16 +150,16 @@ final class Form8949 extends Command
         return $this->getTable()->select(function(Select $select) use ($input)
         {
             $select->join(
-                ['buyHistory' => 'trade_history_btcusd'],
-                'buyHistory.tid = taxes_btcusd_sell_log.buy_tid',
+                ['buyHistory' => "trade_history_{$this->symbol}"],
+                "buyHistory.tid = taxes_{$this->symbol}_sell_log.buy_tid",
                 [
                     'buy_date' => 'trade_date',
                     'buy_price' => 'price'
                 ]
             );
             $select->join(
-                ['saleHistory' => 'trade_history_btcusd'],
-                'saleHistory.tid = taxes_btcusd_sell_log.sell_tid',
+                ['saleHistory' => "trade_history_{$this->symbol}"],
+                "saleHistory.tid = taxes_{$this->symbol}_sell_log.sell_tid",
                 [
                     'sell_date' => 'trade_date',
                     'sell_price' => 'price',
@@ -130,8 +169,8 @@ final class Form8949 extends Command
             $select->where->greaterThanOrEqualTo('saleHistory.trade_date', $year.'-01-01 00:00:00.000');
             $select->where->lessThan('saleHistory.trade_date', ($year + 1).'-01-01 00:00:00.000');
             $select->order([
-                'taxes_btcusd_sell_log.sell_tid ASC',
-                'taxes_btcusd_sell_log.buy_tid ASC'
+                "taxes_{$this->symbol}_sell_log.sell_tid ASC",
+                "taxes_{$this->symbol}_sell_log.buy_tid ASC"
             ]);
         });
     }
@@ -139,7 +178,7 @@ final class Form8949 extends Command
     private function getTable(): TableGateway
     {
         if (!$this->table) {
-            $this->table = new TableGateway('taxes_btcusd_sell_log', Db::getAdapter());
+            $this->table = new TableGateway("taxes_{$this->symbol}_sell_log", Db::getAdapter());
         }
         return $this->table;
     }
