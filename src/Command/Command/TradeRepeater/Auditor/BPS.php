14ce19d8ce14bc1080b99add5da8da442ff43bbc
Darren Felton <darren_felton@hotmail.com>
2020-04-05 10:41:17 -0500

    Improved BPS Auditor some; Formatting

--- src/Command/Command/TradeRepeater/Auditor/BPS.php
+++ src/Command/Command/TradeRepeater/Auditor/BPS.php
@@ -19,10 +19,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Audits the current BPS for Maker orders placed via API.
- * If the BPS is ever detected to be over the maximum threshold expected for
- * trade repeater, this class will initiate an emergency shutdown event so
- * all trading activity is halted. Furthermore, it will use the CancelAllSessionOrdersInterface
- * to cancel all currently open sell orders placed by the TradeRepeater's Seller class.
+ *
+ * If the BPS is ever detected to be over the maximum threshold expected
+ * for trade repeater, this class will initiate an emergency shutdown
+ * event so all trading activity is halted. Furthermore, it will use the
+ * CancelAllSessionOrdersInterface to cancel all currently open sell orders
+ * placed by the TradeRepeater's Seller class.
  */
 final class BPS extends Command
 {
@@ -82,63 +84,88 @@ final class BPS extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $sleep = 0;
         while (!$this->shutdown->isShutdownModeEnabled()) {
-            $this->sleep($sleep, $this->sleeper, $this->shutdown);
             try {
-                $currentBPS = $this->volume->getVolume()->api_maker_fee_bps;
-                if ($currentBPS <= $this->maxBPS->getMaxBPS()) {
-                    $output->writeln("{$this->now()}\tBPS verified to be at or under threshold: <fg=green>$currentBPS</>");
-                    $sleep = self::SLEEP_DELAY;
-                } else {
-                    $this->shutdown->enableShutdownMode(
-                        new \Exception("BPS Threshold Detected ($currentBPS BPS")
-                    );
-                    $output->writeln([
-                        "<fg=red>{$this->now()}\tWARNING: BPS Detected to be over threshold. Current BPS: $currentBPS</>",
-                        "<fg=red>{$this->now()}\tInitiating cancellation of all trade repeater sell orders</>"
-                    ]);
-                    // TODO: We should sleep the max allowed time for our curl calls in case Seller is in the middle of placing an order.
-                    $this->cancelSellOrders();
-                }
-            } catch (ConnectionException $e) {
+                $this->auditBPS($output);
+            } catch (ConnectionException | MaintenanceException | SystemException $e) {
                 $output->writeln("<fg=red>{$this->now()}\t{$e->getMessage()}</>");
-                $sleep = self::SLEEP_DELAY;
-            } catch (MaintenanceException $e) {
-                $output->writeln("<fg=red>{$this->now()}\t{$e->getMessage()}</>");
-                $sleep = self::SLEEP_DELAY;
-            } catch (SystemException $e) {
-                $output->writeln("<fg=red>{$this->now()}\t{$e->getMessage()}</>");
-                $sleep = self::SLEEP_DELAY;
+                $this->sleep(self::SLEEP_DELAY, $this->sleeper, $this->shutdown);
             } catch (\Exception $e) {
                 $this->shutdown->enableShutdownMode($e);
             }
         }
-
         $output->writeln("\n<fg=red>{$this->now()}\tShutdown signal detected.\n");
     }
 
-    private function cancelSellOrders(): array
+    /**
+     * Checks the current maker fee bps for API orders and if it is determined
+     * to be over the allowed threshold for Trade Repeater orders, an emergency
+     * shutdown is enabled and all session sell orders are cancelled so that
+     * they can be manually assessed and re-opened with an appropriate sell price.
+     *
+     * TODO: Someday auto-calculate new sell price. Feasible we eventually have the volume
+     * necessary to have a lower maker BPS fee, and feasible that instead of simply accepting
+     * more earnings, we want to trade on smaller price swings. However should our volume
+     * lose the lower fee tier, we'd want to cancel and re-adjust to higher margin for
+     * sell price.
+     * TODO: Describe the above todo (lol todo on a todo) better in a GitHub issue.
+     *
+     * @param OutputInterface $output
+     */
+    private function auditBPS(OutputInterface $output): void
+    {
+        $currentBPS = $this->volume->getVolume()->api_maker_fee_bps;
+        if ($currentBPS <= $this->maxBPS->getMaxBPS()) {
+            $output->writeln("{$this->now()}\tBPS verified to be at or under threshold: <fg=green>$currentBPS</>");
+            $this->sleep(self::SLEEP_DELAY, $this->sleeper, $this->shutdown);
+        } else {
+            $this->shutdown->enableShutdownMode(
+                new \Exception("BPS Threshold Detected ($currentBPS BPS")
+            );
+            $output->writeln([
+                "<fg=red>{$this->now()}\tWARNING: BPS Detected to be over threshold. Current BPS: $currentBPS</>",
+                "<fg=red>{$this->now()}\tInitiating cancellation of all trade repeater sell orders</>"
+            ]);
+            // TODO: Cheap way of logging order IDs to file. Should probably write these elsewhere.
+            $this->shutdown->enableShutdownMode(
+                new \Exception(\sprintf(
+                    'Trade Repeater Order IDs cancelled: %s',
+                    implode(',', $this->cancelSellOrders())
+                ))
+            );
+        }
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @return array
+     */
+    private function cancelSellOrders(OutputInterface $output): array
     {
         $msg = null;
-        $i = 0;
         do {
-            ++$i;
             try {
                 $msg = $this->cancel->cancelSessionOrders();
-            } catch (InvalidNonceException $e) {
-                // swallow exception. Could have clashed with Seller command on a last attempted order.
-            } catch (ConnectionException $e) {
-                // swallow exception
-            } catch (MaintenanceException $e) {
-                // now what the fuck?
-            } catch (SystemException $e) {
-
+            } catch (InvalidNonceException | ConnectionException | MaintenanceException | SystemException $e) {
+                $output->writeln("<fg=red>{$this->now()}\tException: {$e->getMessage()}");
+                $output->writeln(\sprintf(
+                    "<fg=red>%s\tSleeping %d second(s) before attempting again to cancel all session orders.</>",
+                    $this->now(),
+                    self::SLEEP_EXCEPTION_DELAY
+                ));
+                $this->sleep(self::SLEEP_EXCEPTION_DELAY, $this->sleeper, $this->shutdown);
+            } catch (\Exception $e) {
+                $output->writeln(\sprintf(
+                    "<fg=red>%s\tERROR: TRADE REPEATER SESSION ORDERS LEFT OPEN.</>",
+                    $this->now(),
+                ));
             }
-        } while ($msg = null && $i < 100);
+        } while ($msg = null);
+        $orderIds = [];
         foreach ($msg->details->cancelledOrders as $orderId) {
+            $orderIds[] = $orderId;
         }
-
+        return $orderIds;
     }
 
     private function now(): string
