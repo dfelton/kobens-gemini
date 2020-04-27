f0b91bd1143935162d2766d4826233c4467a43c0
Darren Felton <darren.felton@thegrommet.com>
2020-04-26 19:24:23 -0500

    Spelling Correction

--- src/Command/Command/FeeAndVolume/GetNotionalVolume.php
+++ src/Command/Command/FeeAndVolume/GetNotionalVolume.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class GetNotationalVolume extends Command
+final class GetNotionalVolume extends Command
 {
     protected static $defaultName = 'fee-volume:get-notional-volume';
 
