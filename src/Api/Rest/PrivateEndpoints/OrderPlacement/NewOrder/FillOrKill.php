5a49c5707e437d207415b262bbd3f7fb4662f7c3
Darren Felton <darren_felton@hotmail.com>
2020-05-11 08:51:55 -0500

    use private request interface

--- src/Api/Rest/PrivateEndpoints/OrderPlacement/NewOrder/FillOrKill.php
+++ src/Api/Rest/PrivateEndpoints/OrderPlacement/NewOrder/FillOrKill.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kobens\Gemini\Api\Rest\PrivateEndpoints\OrderPlacement\NewOrder;
 
 use Kobens\Exchange\PairInterface;
+use Kobens\Gemini\Api\Rest\PrivateEndpoints\RequestInterface;
 
 /**
  * This order will only remove liquidity from the order book.
@@ -12,11 +13,21 @@ use Kobens\Exchange\PairInterface;
  * If the order doesn't fully fill immediately, the response back from the API will indicate that the
  * order has already been canceled ("is_cancelled": true in JSON).
  */
-final class FillOrKill extends AbstractNewOrder implements FillOrKillInterface
+final class FillOrKill implements FillOrKillInterface
 {
+    private const URL_PATH = '/v1/order/new';
+
+    private RequestInterface $request;
+
+    public function __construct(
+        RequestInterface $requestInterface
+    ) {
+        $this->request = $requestInterface;
+    }
+
     public function place(PairInterface $pair, string $side, string $amount, string $price, string $clientOrderId = null): \stdClass
     {
-        $this->payload = [
+        $payload = [
             'type' => 'exchange limit',
             'options' => ['fill-or-kill'],
             'symbol' => $pair->getSymbol(),
@@ -25,8 +36,9 @@ final class FillOrKill extends AbstractNewOrder implements FillOrKillInterface
             'side'   => $side,
         ];
         if ($clientOrderId) {
-            $this->payload['client_order_id'] = $clientOrderId;
-        };
-        return \json_decode($this->getResponse()->getBody());
+            $payload['client_order_id'] = $clientOrderId;
+        }
+        $response = $this->request->getResponse(self::URL_PATH, $payload);
+        return \json_decode($response->getBody());
     }
 }
