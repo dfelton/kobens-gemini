b632711eea72c5d989c4caf36350b27675107fad
Darren Felton <darren_felton@hotmail.com>
2020-05-10 23:09:10 -0500

    use request interface

--- src/Api/Rest/PrivateEndpoints/FeeAndVolume/GetNotionalVolume.php
+++ src/Api/Rest/PrivateEndpoints/FeeAndVolume/GetNotionalVolume.php
@@ -4,25 +4,24 @@ declare(strict_types=1);
 
 namespace Kobens\Gemini\Api\Rest\PrivateEndpoints\FeeAndVolume;
 
-use Kobens\Gemini\Api\Rest\PrivateEndpoints\AbstractPrivateRequest;
 use Kobens\Gemini\Api\Rest\PrivateEndpoints\FeeAndVolume\GetNotionalVolume\Response;
+use Kobens\Gemini\Api\Rest\PrivateEndpoints\RequestInterface;
 
-final class GetNotionalVolume extends AbstractPrivateRequest implements GetNotionalVolumeInterface
+final class GetNotionalVolume implements GetNotionalVolumeInterface
 {
     private const URL_PATH = '/v1/notionalvolume';
 
+    private RequestInterface $request;
+
+    public function __construct(
+        RequestInterface $requestInterface
+    ) {
+        $this->request = $requestInterface;
+    }
+
     public function getVolume(): Response
     {
-        return new Response($this->getResponse()->getBody());
-    }
-
-    protected function getUrlPath(): string
-    {
-        return self::URL_PATH;
-    }
-
-    protected function getPayload(): array
-    {
-        return [];
+        $response = $this->request->getResponse(self::URL_PATH, [], [], true);
+        return new Response($response->getBody());
     }
 }
