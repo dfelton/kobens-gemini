fbad5a4cd973e91b81f3d165ab933c209a9aab2c
Darren Felton <darren_felton@hotmail.com>
2020-05-15 09:34:59 -0500

    return array of tables

--- src/Command/Command/TradeRepeater/Watcher.php
+++ src/Command/Command/TradeRepeater/Watcher.php
@@ -132,7 +132,7 @@ final class Watcher extends Command
             $data[] = TradeSpread::getTable($output, $this->data, $symbol);
             $data[] = MarketSpread::getTable($output, $this->data, $symbol);
         } catch (\Kobens\Gemini\Exception $e) {
-            $data[] = $e->getMessage();
+            $data[] = (new Table())->addRow([$e->getMessage()]);
         }
         return $data;
     }
