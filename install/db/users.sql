3563640faf46a931d466339176fda64040b6a8c4
Darren Felton <darren.felton@thegrommet.com>
2020-06-15 21:27:27 -0500

    add drop to taxes table
    necessary for TRUNCATE command

--- install/db/users.sql
+++ install/db/users.sql
@@ -3,6 +3,15 @@
 CREATE USER gemini_prod@'%' IDENTIFIED BY 'FOO_BAR';
 GRANT INSERT, UPDATE, SELECT ON gemini_prod.* TO gemini_prod@'%';
 
+GRANT DROP ON gemini_prod.taxes_btcusd_buy_log TO gemini_prod@'%';
+GRANT DROP ON gemini_prod.taxes_btcusd_sell_log TO gemini_prod@'%';
+
+GRANT DROP ON gemini_prod.taxes_ethusd_buy_log TO gemini_prod@'%';
+GRANT DROP ON gemini_prod.taxes_ethusd_sell_log TO gemini_prod@'%';
+
+GRANT DROP ON gemini_prod.taxes_zecusd_buy_log TO gemini_prod@'%';
+GRANT DROP ON gemini_prod.taxes_zecusd_sell_log TO gemini_prod@'%';
+
 -- gemini_throttler
 CREATE USER gemini_throttler@'%' IDENTIFIED BY 'FOO_BAR';
 GRANT SELECT ON gemini_throttler.throttler TO gemini_throttler@'%';
