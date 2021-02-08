a1a787c72f7643c10f18fe3b4f2a17fa3ffdcb6b
Darren Felton <darren_felton@hotmail.com>
2021-02-08 17:47:25 +0000

    Invalid Minimum Amount Exception for order

---
+++ src/Exception/Order/MinimumAmountException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kobens\Gemini\Exception\Order;
+
+class MinimumAmountException extends \InvalidArgumentException
+{
+    public function __construct()
+    {
+
+    }
+}
