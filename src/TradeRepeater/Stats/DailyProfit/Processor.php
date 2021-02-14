0d7e61dc7eb744a7d9ca2d465e17fa6511f4dbe7
Darren Felton <darren_felton@hotmail.com>
2021-02-14 05:34:00 +0000

    Tracks daily profit of currencies

---
+++ src/TradeRepeater/Stats/DailyProfit/Processor.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kobens\Gemini\TradeRepeater\Stats\DailyProfit;
+
+use Kobens\Gemini\TradeRepeater\Stats\ProcessorInterface;
+use Zend\Db\Adapter\Adapter;
+
+final class Processor implements ProcessorInterface
+{
+    private Adapter $adapter;
+
+    public function __construct(
+
+    ) {
+
+    }
+
+    public function execute(): void
+    {
+
+    }
+}
