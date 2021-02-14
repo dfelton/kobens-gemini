005b706f8e5606d95f9aad43417a0f7c60398d5e
Darren Felton <darren_felton@hotmail.com>
2021-02-14 05:28:59 +0000

    Store profits in bucket, will pick up and use in a process not tied to archiver

--- src/TradeRepeater/Model/Trade/Profits/Processor.php
+++ src/TradeRepeater/Model/Trade/Profits/Processor.php
@@ -6,10 +6,8 @@ namespace Kobens\Gemini\TradeRepeater\Model\Trade\Profits;
 
 use Kobens\Gemini\TradeRepeater\Model\Trade;
 use Kobens\Gemini\TradeRepeater\Model\Trade\Profits\Processor\ReInvest;
-use Kobens\Gemini\TradeRepeater\Model\Resource\Trade as TradeResource;
 use Kobens\Gemini\TradeRepeater\Model\Trade\CalculateCompletedProfits;
 use Kobens\Gemini\Exchange\Currency\Pair;
-use Kobens\Gemini\TradeRepeater\Model\Trade\Profits\Processor\TopWeekly;
 
 /**
  * When a repeater position has completed a sale, we have
@@ -22,31 +20,25 @@ use Kobens\Gemini\TradeRepeater\Model\Trade\Profits\Processor\TopWeekly;
  */
 final class Processor implements ProcessorInterface
 {
-    private TopWeekly $topWeekly;
+    private Bucket $bucket;
 
     private ReInvest $reinvest;
 
     public function __construct(
         ReInvest $reinvest,
-        TradeResource $tradeResource,
-        TopWeekly $topWeekly
+        Bucket $bucket
     ) {
+        $this->bucket = $bucket;
         $this->reinvest = $reinvest;
-        $this->topWeekly = $topWeekly;
     }
 
     public function execute(Trade $trade): void
     {
-        $pair = Pair::getInstance($trade->getSymbol());
-        if ($pair->getQuote()->getSymbol() !== 'usd') {
-            return;
-        }
-
+        $quote = Pair::getInstance($trade->getSymbol())->getQuote()->getSymbol();
+        $base = Pair::getInstance($trade->getSymbol())->getBase()->getSymbol();
         $profits = CalculateCompletedProfits::get($trade);
-        $quoteAmount = $profits[$pair->getQuote()->getSymbol()];
-        $quoteAmount = $this->reinvest->execute($quoteAmount, $trade);
-        $quoteAmount = $this->topWeekly->execute($quoteAmount);
-
-        // TODO: put the remaining $quoteAmount and $profits[$pair->getBase()->getSymbol()] into a db table "bucket" for use by a future cron
+        $this->bucket->addToBucket($quote, $this->reinvest->execute($profits[$quote], $trade));
+        $this->bucket->addToBucket($base, $profits[$base]);
     }
+
 }
