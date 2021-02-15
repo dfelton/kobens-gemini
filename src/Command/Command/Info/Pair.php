2f81746cee7e12f478cafcf5c24acbd3aeba9730
Darren Felton <darren_felton@hotmail.com>
2021-02-15 17:39:40 +0000

    Condense table output, add filtering feature

--- src/Command/Command/Info/Pair.php
+++ src/Command/Command/Info/Pair.php
@@ -10,24 +10,38 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Kobens\Gemini\Exchange\Currency\Pair as P;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputOption;
 
+/**
+ * Meant to output similar table as found here: https://docs.gemini.com/rest-api/#symbols-and-minimums
+ */
 final class Pair extends Command
 {
     protected function configure(): void
     {
         $this->setName('info:pair');
         $this->setDescription('Outputs information about a particular trading pair');
-        $this->addArgument('pair', InputArgument::REQUIRED, 'Pair to fetch info about');
+        $this->addArgument('pair', InputArgument::OPTIONAL, 'Pair to fetch info about. Omit to output information on all pairs.');
+        $this->addOption('quote', null, InputOption::VALUE_OPTIONAL, 'Fetch pairs with given quote currency.');
+        $this->addOption('base', null, InputOption::VALUE_OPTIONAL, 'Fetch pairs with given base currency.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $exitCode = 0;
         try {
-            $this->getTable(
-                $output,
-                P::getInstance($input->getArgument('pair'))
-            )->render();
+            $pairs = $this->getPairs($input);
+            $table = new Table($output);
+            $table->setHeaders([
+                'Symbol',
+                'Minimum Order Size',
+                'Tick Size',
+                'Quote Currency Price Increment',
+            ]);
+            foreach ($this->getData($pairs) as $row) {
+                $table->addRow($row);
+            }
+            $table->render();
         } catch (\Throwable $e) {
             $exitCode = 1;
             $output->writeln([
@@ -39,39 +53,65 @@ final class Pair extends Command
         return $exitCode;
     }
 
-    private function getTable(OutputInterface $output, P $pair): Table
+    /**
+     * @param InputInterface $input
+     * @return P[]
+     */
+    private function getPairs(InputInterface $input): array
     {
-        $base = strtoupper($pair->getBase()->getSymbol());
-        $quote = strtoupper($pair->getQuote()->getSymbol());
-        $table = new Table($output);
-        $table->addRow([
-            'Minimum Order Increment',
-            sprintf(
-                '%s %s%s',
-                $pair->getMinOrderIncrement(),
-                $base,
-                $this->getENotation($pair->getMinOrderIncrement())
-            ),
-        ]);
-        $table->addRow([
-            'Minimum Order Size',
-            sprintf(
-                '%s %s%s',
-                $pair->getMinOrderSize(),
-                $base,
-                $this->getENotation($pair->getMinOrderSize())
-            ),
-        ]);
-        $table->addRow([
-            'Minimum Price Increment',
-            sprintf(
-                '%s %s%s',
-                $pair->getMinPriceIncrement(),
-                $quote,
-                $this->getENotation($pair->getMinPriceIncrement())
-            )
-        ]);
-        return $table;
+        $pairs = is_string($input->getArgument('pair'))
+            ? [P::getInstance($input->getArgument('pair'))]
+            : P::getAllInstances();
+        if (is_string($input->getOption('quote'))) {
+            $quote = strtolower($input->getOption('quote'));
+            foreach ($pairs as $i => $pair) {
+                if ($pair->getQuote()->getSymbol() !== $quote) {
+                    unset($pairs[$i]);
+                }
+            }
+        }
+        if (is_string($input->getOption('base'))) {
+            $base = strtolower($input->getOption('base'));
+            foreach ($pairs as $i => $pair) {
+                if ($pair->getBase()->getSymbol() !== $base) {
+                    unset($pairs[$i]);
+                }
+            }
+        }
+        return array_values($pairs);
+    }
+
+    /**
+     * @param P[] $pairs
+     * @return array
+     */
+    private function getData(array $pairs): \Generator
+    {
+        foreach ($pairs as $pair) {
+            $base = strtoupper($pair->getBase()->getSymbol());
+            $quote = strtoupper($pair->getQuote()->getSymbol());
+            yield [
+                strtoupper($pair->getSymbol()),
+                sprintf(
+                    '%s %s%s',
+                    $pair->getMinOrderSize(),
+                    $base,
+                    $this->getENotation($pair->getMinOrderSize())
+                ),
+                sprintf(
+                    '%s %s%s',
+                    $pair->getMinOrderIncrement(),
+                    $base,
+                    $this->getENotation($pair->getMinOrderIncrement())
+                ),
+                sprintf(
+                    '%s %s%s',
+                    $pair->getMinPriceIncrement(),
+                    $quote,
+                    $this->getENotation($pair->getMinPriceIncrement())
+                )
+            ];
+        }
     }
 
     private function getENotation(string $num): string
