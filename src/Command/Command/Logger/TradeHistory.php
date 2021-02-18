6992e32136e3be329dbafbca2247a8b2b4a1d47a
Darren Felton <darren_felton@hotmail.com>
2021-02-18 05:23:07 +0000

    misc tech tech improvements
    - No more examining the exception thrown, instead we try to select the record to test if it exists or not.
    - utilize kill file for ability to target processes of this job type for shutting down
    - Ping the database when no results, as we do not want it to hang up on us

--- src/Command/Command/Logger/TradeHistory.php
+++ src/Command/Command/Logger/TradeHistory.php
@@ -13,23 +13,32 @@ use Kobens\Core\EmergencyShutdownInterface;
 use Kobens\Core\SleeperInterface;
 use Kobens\Core\Exception\ConnectionException;
 use Kobens\Gemini\Api\Rest\PrivateEndpoints\OrderStatus\GetPastTradesInterface;
+use Kobens\Gemini\Command\Traits\GetNow;
+use Kobens\Gemini\Command\Traits\GetIntArg;
+use Kobens\Gemini\Command\Traits\KillFile;
+use Kobens\Gemini\Command\Traits\TradeRepeater\ExitProgram;
 use Kobens\Gemini\Exchange\Currency\Pair;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Zend\Db\Adapter\Exception\InvalidQueryException;
 use Zend\Db\Sql\Select;
 use Zend\Db\TableGateway\TableGateway;
-use Kobens\Gemini\Command\Traits\GetNow;
+use Zend\Db\Adapter\Adapter;
 
 final class TradeHistory extends Command
 {
+    use KillFile;
+    use ExitProgram;
     use GetNow;
+    use GetIntArg;
 
     protected static $defaultName = 'logger:trade-history';
 
-    private const DEFAULT_SYMBOL = 'btcusd';
+    private const KILL_FILE = 'kill_logger_trade_history';
+
     private const DELAY_DEFAULT  = 60;
     private const MIN_DELAY      = 30;
     private const MAX_DELAY      = 600;
@@ -48,21 +57,27 @@ final class TradeHistory extends Command
 
     private Pair $pair;
 
+    private Adapter $adapter;
+
+    private ?int $lastInsertedTransactionId = null;
+
     public function __construct(
         GetPastTradesInterface $getPastTradesInterface,
         EmergencyShutdownInterface $shutdownInterface,
-        SleeperInterface $sleeper
+        SleeperInterface $sleeper,
+        Adapter $adapter
     ) {
         $this->pastTrades = $getPastTradesInterface;
         $this->shutdown = $shutdownInterface;
         $this->sleeper = $sleeper;
+        $this->adapter = $adapter;
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this->setDescription('Maintains The trading history for a specified symbol.');
-        $this->addOption('symbol', 's', InputOption::VALUE_OPTIONAL, 'Symbol to fetch history for.', self::DEFAULT_SYMBOL);
+        $this->addArgument('symbol', InputArgument::REQUIRED, 'Symbol to fetch history for.');
         $this->addOption(
             'delay',
             'd',
@@ -79,30 +94,17 @@ final class TradeHistory extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $exitCode = 0;
-        $this->symbol = $input->getOption('symbol');
+        $this->symbol = $input->getArgument('symbol');
         $this->pair = Pair::getInstance($this->symbol);
-        $this->delay  = (int) $input->getOption('delay');
+        $this->delay = $this->getIntArg($input, 'delay', self::DELAY_DEFAULT, self::MIN_DELAY, self::MAX_DELAY);
         $timestampms  = $this->getLastTradeTimestampMs();
-
-        if ($this->delay < self::MIN_DELAY || $this->delay > self::MAX_DELAY) {
-            $this->delay = self::DEFAULT_DELAY;
-        }
-
         try {
             $this->mainLoop($output, $timestampms);
-        } catch (\Exception $e) {
+        } catch (\Throwable $e) {
             $this->shutdown->enableShutdownMode($e);
             $exitCode = 1;
         }
-        if ($this->shutdown->isShutdownModeEnabled()) {
-            $output->writeln(sprintf(
-                "<fg=red>%s\tShutdown signal detected - %s (%s)",
-                $this->getNow(),
-                self::class,
-                $this->symbol
-            ));
-        }
-
+        $this->outputExit($output, $this->shutdown, self::KILL_FILE, sprintf(' (%s)', $this->symbol));
         return $exitCode;
     }
 
@@ -126,37 +128,35 @@ final class TradeHistory extends Command
                 $output->writeln("<fg=red>{$this->getNow()}\tError Code: {$e->getCode()}</>");
                 $output->writeln("<fg=red>{$this->getNow()}\tError: {$e->getMessage()}</>");
                 $output->writeln("<fg=red>{$this->getNow()}\tSleeping 10 seconds and trying again...</>");
-                \sleep(10);
+                $this->sleeper->sleep(
+                    10,
+                    function (): bool {
+                        return $this->shutdown->isShutdownModeEnabled();
+                    }
+                );
                 continue;
-            } catch (\Exception $e) {
-                $this->shutdown->enableShutdownMode($e);
-                break;
             }
 
             $i = \count($page);
-            $hadResults = $i > 0 ? true : false;
+            $hadResults = false;
 
             while ($i > 0) {
                 --$i;
-                try {
-                    if ($this->logTrade($page[$i])) {
-                        $output->writeln(sprintf(
-                            "%s\tTransaction %d Logged: %s %s %s @ %s %s/%s on %s for order id %s",
-                            $this->getNow(),
-                            $page[$i]->tid,
-                            $page[$i]->type,
-                            $page[$i]->amount,
-                            strtoupper($this->pair->getBase()->getSymbol()),
-                            $page[$i]->price,
-                            strtoupper($this->pair->getBase()->getSymbol()),
-                            strtoupper($this->pair->getQuote()->getSymbol()),
-                            date('Y-m-d H:i:s', $page[$i]->timestamp),
-                            $page[$i]->order_id
-                        ));
-                    }
-                } catch (\Exception $e) {
-                    $this->shutdown->enableShutdownMode($e);
-                    break;
+                if ($this->logTrade($page[$i])) {
+                    $output->writeln(sprintf(
+                        "%s\tTransaction %d Logged: %s %s %s @ %s %s/%s on %s for order id %s",
+                        $this->getNow(),
+                        $page[$i]->tid,
+                        $page[$i]->type,
+                        $page[$i]->amount,
+                        strtoupper($this->pair->getBase()->getSymbol()),
+                        $page[$i]->price,
+                        strtoupper($this->pair->getBase()->getSymbol()),
+                        strtoupper($this->pair->getQuote()->getSymbol()),
+                        date('Y-m-d H:i:s', $page[$i]->timestamp),
+                        $page[$i]->order_id
+                    ));
+                    $hadResults = true;
                 }
             }
 
@@ -171,9 +171,11 @@ final class TradeHistory extends Command
                             //     'Unable to ensure we can fetch the next page. Maximum results per page yielded trade all executing on the same timestamp. Timestamp Milliseconds: '.$timestampms
                             // );
                             $this->logPageLimitError($pageFirstTimestampms);
-                            $output->writeln(
-                                "\n{$this->getNow()}\t<fg=red>SKIPPING POTENTIAL $pageFirstTimestampms ORDERS DUE TO MAX PAGE SIZE LIMIT.</>"
-                            );
+                            $output->writeln(sprintf(
+                                "%s\t<fg=red>SKIPPING POTENTIAL %d ORDERS DUE TO MAX PAGE SIZE LIMIT.</>",
+                                $this->getNow(),
+                                $timestampms
+                            ));
                         }
                     }
                 } else {
@@ -185,9 +187,16 @@ final class TradeHistory extends Command
                             $this->delay
                         ));
                     }
-                    $this->sleeper->sleep($this->delay, function (): bool {
-                        return $this->shutdown->isShutdownModeEnabled();
-                    });
+
+                    // Cheap way to ping the database before we sleep.
+                    $this->adapter->query('SELECT 1')->execute();
+
+                    $this->sleeper->sleep(
+                        $this->delay,
+                        function (): bool {
+                            return $this->shutdown->isShutdownModeEnabled();
+                        }
+                    );
                 }
             }
         }
@@ -203,32 +212,41 @@ final class TradeHistory extends Command
 
     private function logTrade(\stdClass $trade): bool
     {
-        $result = false;
-        try {
-            $this->getTable()->insert([
-                'tid' => $trade->tid,
-                'price' => $trade->price,
-                'amount' => $trade->amount,
-                'timestampms' => $trade->timestampms,
-                'type' => \strtolower($trade->type),
-                'aggressor' => $trade->aggressor ? 1 : 0,
-                'fee_currency' => $trade->fee_currency,
-                'fee_amount' => $trade->fee_amount,
-                'order_id' => $trade->order_id,
-                'client_order_id' => \property_exists($trade, 'client_order_id') ? $trade->client_order_id : null,
-                'trade_date' => \gmdate('Y-m-d H:i:s', $trade->timestamp),
-            ]);
-            $result = true;
-        } catch (InvalidQueryException $e) {
-            $previous = $e->getPrevious();
-            if ($previous instanceof \PDOException && $previous->getCode() === '23000') {
-                // TODO: This may cause us to go over our max_error_count setting, we should do a select first
-                // just means we logged it already the prior fetched page
-            } else {
-                throw $e;
-            }
+        if ($this->transactionExists($trade->tid)) {
+            return false;
         }
-        return $result;
+        $this->getTable()->insert([
+            'tid' => $trade->tid,
+            'price' => $trade->price,
+            'amount' => $trade->amount,
+            'timestampms' => $trade->timestampms,
+            'type' => \strtolower($trade->type),
+            'aggressor' => $trade->aggressor ? 1 : 0,
+            'fee_currency' => $trade->fee_currency,
+            'fee_amount' => $trade->fee_amount,
+            'order_id' => $trade->order_id,
+            'client_order_id' => \property_exists($trade, 'client_order_id') ? $trade->client_order_id : null,
+            'trade_date' => \gmdate('Y-m-d H:i:s', $trade->timestamp),
+        ]);
+        $this->lastInsertedTransactionId = $trade->tid;
+        return true;
+    }
+
+    private function transactionExists(int $tid): bool
+    {
+        $exists = false;
+        if ($this->lastInsertedTransactionId === null) {
+            $result = $this->getTable()->select(function (Select $select) use ($tid): void {
+                $select->where->equalTo('tid', $tid);
+            });
+            $exists = $result->count() === 1;
+            if ($exists) {
+                $this->lastInsertedTransactionId = $tid;
+            }
+        } else {
+            $exists = $tid === $this->lastInsertedTransactionId;
+        }
+        return $exists;
     }
 
     /**
