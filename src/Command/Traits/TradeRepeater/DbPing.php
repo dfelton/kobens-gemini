a8fb573e24e3d387a72d0692036140660e7718dd
Darren Felton <darren_felton@hotmail.com>
2021-03-07 03:14:56 +0000

    Ping the connection used for throttling. Fixes #131

---
+++ src/Command/Traits/TradeRepeater/DbPing.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kobens\Gemini\Command\Traits\TradeRepeater;
+
+use Zend\Db\Adapter\Adapter;
+
+trait DbPing
+{
+    /**
+     * TODO: Eliminate this via design pattern eventually. We should be able to just reconnect
+     * in most scenario rather than repeatedly pinging the database.
+     *
+     * @param Adapter $adapter
+     */
+    private function ping(Adapter $adapter): void
+    {
+        $adapter->query('SELECT "" AS ping');
+    }
+}
