7d3f2b54ef163730b8e157dce8813e84c1f44bb3
Darren Felton <darren_felton@hotmail.com>
2021-11-14 05:59:23 +0000

    Add FETUSD

--- install/db/schema_history.sql
+++ install/db/schema_history.sql
@@ -449,6 +449,21 @@ CREATE TABLE IF NOT EXISTS `trade_history_ethusd` (
   PRIMARY KEY (`tid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Trade History ETHUSD';
 
+CREATE TABLE IF NOT EXISTS `trade_history_fetusd` (
+  `tid` bigint(13) unsigned NOT NULL COMMENT 'Transaction ID',
+  `price` varchar(50) NOT NULL COMMENT 'Price',
+  `amount` varchar(50) NOT NULL COMMENT 'Amount',
+  `timestampms` bigint(13) unsigned NOT NULL COMMENT 'Timestamp Milliseconds',
+  `type` varchar(4) NOT NULL COMMENT 'Type',
+  `aggressor` tinyint(1) NOT NULL COMMENT 'Aggressor',
+  `fee_currency` varchar(10) NOT NULL COMMENT 'Fee Currency',
+  `fee_amount` varchar(50) NOT NULL COMMENT 'Fee Amount',
+  `order_id` bigint(13) unsigned NOT NULL COMMENT 'Order Id',
+  `client_order_id` varchar(100) DEFAULT NULL,
+  `trade_date` timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Trade Date',
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Trade History FETUSD';
+
 CREATE TABLE IF NOT EXISTS `trade_history_filusd` (
   `tid` bigint(13) unsigned NOT NULL COMMENT 'Transaction ID',
   `price` varchar(50) NOT NULL COMMENT 'Price',
